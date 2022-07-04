addc5719fdae5207cd0a963421b5b208a83a44bf
Diego Caiza <slayercorp2017@gmail.com>
2022-07-04 16:33:34 -0500

    control-props-alt

--- src/02-component-patterns/hooks/useProduct.ts
+++ src/02-component-patterns/hooks/useProduct.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useState } from "react"
 import { Product, onChangeArgs } from '../interfaces/interfaces';
 
 
@@ -11,11 +11,8 @@ interface useProductArgs {
 export const useProduct = ({onChange, product, value = 0}: useProductArgs) => {
   const [counter, setcounter] = useState(value);
    
-  const isControlled = useRef(!!onChange)
   const increaseBy = (value: number) => {
-    if(isControlled.current){
-      return onChange!({count:value, product})
-    }
+
     const newValue = Math.max(counter +  value, 0)
     setcounter(prev => Math.max(prev +  value, 0))
     onChange && onChange({count: newValue, product });
--- src/02-component-patterns/hooks/useShoppingCart.ts
+++ src/02-component-patterns/hooks/useShoppingCart.ts
@@ -16,17 +16,15 @@ import { products } from '../data/products';
         product: Product;
       }) => {
         setShoppingCart((oldShoppingCart) => {
-          const productInCart : ProductInCart = oldShoppingCart[product.id] || {...product, count:0};
     
-          if( Math.max(productInCart.count + count , 0) > 0){
-            productInCart.count += count;
-            return {
-              ...oldShoppingCart, [product.id]: productInCart
-            }
-          }
-          //borrar product
-          const { [product.id]: toDelete, ...rest } = oldShoppingCart;
+          if( count === 0){
+            const { [product.id]: toDelete, ...rest } = oldShoppingCart;
           return { ...rest };
+          }
+          return{
+              ...oldShoppingCart,
+              [product.id]: {...product, count}
+          }
         }
         
         )
